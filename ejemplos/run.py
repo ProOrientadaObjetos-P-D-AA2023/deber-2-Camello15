9c4b7cd25baaf88fbc72b1784f7912b525349866
Camello15 <116181666+Camello15@users.noreply.github.com>
2023-05-14 12:54:31 -0500

    Update run.py

--- ejemplos/run.py
+++ ejemplos/run.py
@@ -9,6 +9,20 @@
 
 # Presentar objeto; usar el método __st__
 
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def __str__(self):
+        return f"Nombre: {self.nombre}, Edad: {self.edad}"
+      
+      persona1 = Persona("Juan", 30)
+persona2 = Persona("Maria", 25)
+
+print(persona1)
+print(persona2)
+
 # objeto 02
 
 # crear ingresando valores por teclado
